03eef1935fe769a83533d78b0d6a5d1d8b65b5e3
Илья Красильников <kras-05@mail.ru>
2015-11-15 17:18:24 +0300

    Обновление после проведение окончательного тестирования.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Triangle
+{
+    partial class Triangle
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(76, 44);
+            this.txtA.MaxLength = 25;
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(148, 20);
+            this.txtA.TabIndex = 0;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(76, 70);
+            this.txtB.MaxLength = 25;
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(148, 20);
+            this.txtB.TabIndex = 1;
+            this.txtB.TextChanged += new System.EventHandler(this.txtB_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Параметры сторон\r\nпрямоугольного треугольника:";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 47);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(62, 13);
+            this.lblA.TabIndex = 3;
+            this.lblA.Text = "Сторона А:";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(12, 73);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(62, 13);
+            this.lblB.TabIndex = 4;
+            this.lblB.Text = "Сторона Б:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 98);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(62, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Результат:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(76, 95);
+            this.txtResult.MaxLength = 32000;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(148, 20);
+            this.txtResult.TabIndex = 6;
+            // 
+            // Triangle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 120);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Triangle";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Треугольник";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Triangle_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Triangle
+{
+    public partial class Triangle : Form
+    {
+        string[] IgnoreStrs = { "e+", "e-", "e" };
+        string StrA = string.Empty, StrB = string.Empty;
+
+        public Triangle()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Вычисляет площадь прямоугольного треугольника
+        /// </summary>
+        /// <param name="A">Сторона А</param>
+        /// <param name="B">Сторона Б</param>
+        /// <returns>Возвращает площадь прямоугольного треугольника.
+        /// В случае задания некорректных значений (меньше нуля) выбрасывается исключение ArgumentOutOfRangeException</returns>
+        public static double TriangleSquare(double A, double B)
+        {
+            if (A < 0.0)
+                throw new ArgumentOutOfRangeException("A", A, "Сторона А имеет некорректное значение");
+            if (B < 0.0)
+                throw new ArgumentOutOfRangeException("Б", B, "Сторона Б имеет некорректное значение");
+            return (A * B) / 2.0;
+        }
+
+        /// <summary>
+        /// Возвращает сторону А
+        /// </summary>
+        public double SideA
+        {
+            get
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(txtA.Text))
+                        return 0.0;
+                    foreach (string str in IgnoreStrs)
+                        if (txtA.Text.EndsWith(str, StringComparison.OrdinalIgnoreCase))
+                            return Convert.ToDouble(txtA.Text.Remove(txtA.Text.Length - str.Length));
+                    return Convert.ToDouble(txtA.Text);
+                }
+                catch (Exception ex)
+                {
+                    throw new ArgumentOutOfRangeException("Сторона А", txtA.Text, ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает сторону Б
+        /// </summary>
+        public double SideB
+        {
+            get
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(txtB.Text))
+                        return 0.0;
+                    foreach (string str in IgnoreStrs)
+                        if (txtB.Text.EndsWith(str, StringComparison.OrdinalIgnoreCase))
+                            return Convert.ToDouble(txtB.Text.Remove(txtB.Text.Length - str.Length));
+                    return Convert.ToDouble(txtB.Text);
+                }
+                catch (Exception ex)
+                {
+                    throw new ArgumentOutOfRangeException("Сторона Б", txtB.Text, ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вызывается после ввода параметров. Исключения не выбрасываются
+        /// </summary>
+        void ParsInput()
+        {
+            try
+            {
+                double S = TriangleSquare(SideA, SideB);
+                if (S < 0.0)
+                {
+                    MessageBox.Show(this, "Площадь не может быть меньше нуля: Ошибка в функции - программа будет закрыта", "Неизвестная ошибка");
+                    Application.Exit();
+                }
+                txtResult.Text = (S > 0.0) ? S.ToString() : string.Empty;
+                StrA = txtA.Text;
+                StrB = txtB.Text;
+            }//Порядок обработчиков должен быть именно такой, какой представлен ниже, т.к. в C# порядок обработчиков имеет значение
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(this, ex.Message, "Ошибка");
+                txtA.Text = StrA;
+                txtB.Text = StrB;
+                if (txtA.Focused)
+                    txtA.SelectionStart = txtA.Text.Length;
+                else
+                    if (txtB.Focused)
+                        txtB.SelectionStart = txtB.Text.Length;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Неизвестная ошибка");
+            }
+        }
+
+        /// <summary>
+        /// Происходит при изменении параметра "Сторона А"
+        /// </summary>
+        /// <param name="sender">Ссылка на вызывающий элемент</param>
+        /// <param name="e">Аргументы события</param>
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+            ParsInput();
+        }
+
+        /// <summary>
+        /// Происходит при изменении параметра "Сторона Б"
+        /// </summary>
+        /// <param name="sender">Ссылка на вызывающий элемент</param>
+        /// <param name="e">Аргументы события</param>
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+            ParsInput();
+        }
+
+        /// <summary>
+        /// Происходит при отпускании клавиши над формой
+        /// </summary>
+        /// <param name="sender">Ссылка на вызывающий элемент</param>
+        /// <param name="e">Аргументы события</param>
+        private void Triangle_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+        }
+    }
+}
